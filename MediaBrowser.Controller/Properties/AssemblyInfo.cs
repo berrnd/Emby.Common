ffc96e9044f0d37baddc6c641d7d788d6d509f4e
Bernd Bestel <bernd@berrnd.de>
2018-10-08 19:52:07 +0200

    Convert this to a .Net Framework project Never got it to compile under .Net Core, maybe it's an upstream problem, maybe it's my lack of knowledge about .Net Core...

--- MediaBrowser.Controller/Properties/AssemblyInfo.cs
+++ MediaBrowser.Controller/Properties/AssemblyInfo.cs
@@ -17,6 +17,8 @@ using System.Runtime.InteropServices;
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
+[assembly: AssemblyVersion("3.5.0.0")]
+[assembly: AssemblyFileVersion("3.5.0.0")]
 
 // Version information for an assembly consists of the following four values:
 //
